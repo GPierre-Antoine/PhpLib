29425686357d9ccaf29735632aeb762a3d6229d1
pierreantoine <pierreantoine.guillaume+github@gmail.com>
2018-08-08 16:33:46 +0200

    Inclusion of Experimental Namespace + model +database +persistentStore +resource

---
+++ lib/Experimental/Framework/Resource/PersistentStore.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pierreantoine
+ * Date: 08/08/18
+ * Time: 14:49
+ */
+
+namespace Experimental\Framework\Resource;
+
+
+interface PersistentStore
+{
+
+}
---
+++ lib/Experimental/Framework/Resource/Resource.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pierreantoine
+ * Date: 08/08/18
+ * Time: 14:40
+ */
+
+namespace Experimental\Framework\Resource;
+
+
+interface Resource
+{
+
+}
