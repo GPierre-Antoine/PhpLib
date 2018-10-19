6b3ce0cb31839126885b698276f065cf4fbf3c48
pierreantoine <pierreantoine.guillaume+github@gmail.com>
2018-10-19 17:48:58 +0200

    regex filter iterator

---
+++ lib/PAG/Collection/RegexFilterIterator.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pierreantoine
+ * Date: 19/10/18
+ * Time: 17:48
+ */
+
+namespace PAG\Collection;
+
+
+use Iterator;
+
+class RegexFilterIterator implements \Iterator
+{
+    /**
+     * @var Iterator
+     */
+    private $innerIterator;
+    private $pattern;
+
+    public function __construct(Iterator $innerIterator, $pattern)
+    {
+        $this->innerIterator = $innerIterator;
+        $this->pattern       = $pattern;
+    }
+
+    public function next()
+    {
+        $this->innerIterator->next();
+        while ($this->valid() && !preg_match($this->pattern, $this->current())) {
+            $this->innerIterator->next();
+        }
+    }
+
+    public function valid()
+    {
+        return $this->innerIterator->valid();
+    }
+
+    public function current()
+    {
+        return $this->innerIterator->current();
+    }
+
+    public function key()
+    {
+        return $this->innerIterator->key();
+    }
+
+    public function rewind()
+    {
+        $this->innerIterator->rewind();
+    }
+}
