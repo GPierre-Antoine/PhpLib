9d352b97f8850f41398a00ada63f13c8f51c373e
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2019-08-02 10:48:11 +0200

    :white_check_mark: adding removed array tester

---
+++ test/unit/Core/Datastructure/ArrayTest.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace unit\Core\Datastructure;
+
+
+use PHPUnit\Framework\TestCase;
+
+class ArrayTest extends TestCase
+{
+    public function testArrayKey()
+    {
+        $firstArray = ['key' => 'value'];
+        $secondArray = array_map(function ($item) {
+            return $item;
+        },
+            $firstArray);
+        $this->assertArrayHasKey('key', $secondArray);
+        $thirdArray = array_filter($firstArray,
+            function () {
+                return true;
+            });
+        $this->assertArrayHasKey('key', $thirdArray);
+    }
+
+    public function testConcat()
+    {
+        $first = [2 => 'a', 3 => 'z', 4 => 'e', 'w'];
+        $second = [5 => 'r', 6 => 't', 4 => 'v', 'l'];
+        $this->assertEquals([2 => 'a', 3 => 'z', 4 => 'e', 5 => 'w', 6 => 't', 7 => 'l'], $first + $second);
+        $this->assertEquals(['a', 'z', 'e', 'w', 'r', 't', 'v', 'l'], array_merge($first, $second));
+    }
+}
